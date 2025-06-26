2a2aacc8ae9baedfc913f86ca4a6346a62c494fc
Charan010 <charanmanuri1@gmail.com>
2025-06-26 17:08:43 +0530

    added daemon thread for automatic log compaction

---
+++ src/dev/meshkv/Main.java
@@ -0,0 +1,22 @@
+package src.dev.meshkv;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+    public static void main(String[] args){
+
+        int threadPoolSize = 100; //hard coded for now :/
+        ConcurrentHashMap<ByteBuffer,byte[]> hm = new ConcurrentHashMap<>();
+        KeyValue keyValue = new KeyValue(hm, threadPoolSize);
+
+        //replay from log file on startup
+        LogReplayer.replayLogs(keyValue);
+
+        LogJanitor logJanitor = new LogJanitor("src/dev/meshkv/log/master.log");
+        Thread janitor = new Thread(logJanitor);
+
+        janitor.setDaemon(true);
+        janitor.start();
+    }
+}
